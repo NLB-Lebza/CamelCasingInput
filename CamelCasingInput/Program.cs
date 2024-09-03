0d389d12d02294ca373e5bd9d4a41c99b6061887
mrbal <mrbal@HEREWEGOAGAIN>
2024-09-03 20:18:55 +0200

    Add project files.

---
+++ CamelCasingInput/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Threading.Channels;
+
+public class CamelCasing
+{
+    public static string BreakCamelCase(string str){
+
+        string results = "";
+        for (int i = 0; i < str.Length; i++) 
+        {
+            if (char.IsUpper(str[i]))
+            {
+                results += " ";
+            }
+            results += str[i];
+        }
+
+
+        return results;
+}
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        input=BreakCamelCase(input);
+        Console.WriteLine(input);
+       
+
+    }
+
+
+}
+
+
+
